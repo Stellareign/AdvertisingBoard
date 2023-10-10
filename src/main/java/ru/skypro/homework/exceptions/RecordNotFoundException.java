431c879ba47c746303f8484420d887166777c7b8
Stellareign <tuguran@mail.ru>
2023-10-10 22:06:07 +0300

    Также (ранее) удалены лишние классы

--- src/main/java/ru/skypro/homework/exceptions/RecordNotFoundException.java
+++ src/main/java/ru/skypro/homework/exceptions/RecordNotFoundException.java
@@ -3,8 +3,4 @@ package ru.skypro.homework.exceptions;
 public class RecordNotFoundException extends Throwable {
     public RecordNotFoundException(String s) {
     }
-
-    public RecordNotFoundException() {
-
-    }
 }
