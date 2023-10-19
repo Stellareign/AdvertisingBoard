dc427f6e294e2dd1947eb204fad3b069f8098886
Stellareign <tuguran@mail.ru>
2023-10-19 18:20:23 +0300

    Немного поправок

--- src/main/java/ru/skypro/homework/repository/AdsRepository.java
+++ src/main/java/ru/skypro/homework/repository/AdsRepository.java
@@ -1,11 +1,8 @@
 package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import ru.skypro.homework.dto.ads.Ad;
-import ru.skypro.homework.entity.AdEntity;
 
-import java.util.List;
+import ru.skypro.homework.entity.AdEntity;
 
 
 public interface AdsRepository extends JpaRepository<AdEntity, Integer> {
