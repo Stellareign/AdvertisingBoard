325477cd7a5c2470b454f71f9338e416ce904914
Stellareign <tuguran@mail.ru>
2023-10-25 21:02:15 +0300

    Вернула адрес удаления и обновления комментария

--- src/main/java/ru/skypro/homework/controller/CommentsController.java
+++ src/main/java/ru/skypro/homework/controller/CommentsController.java
@@ -43,7 +43,7 @@ public class CommentsController {
 
     // удаление комментария по id
     @Operation(summary = "Удаление комментария")
-    @DeleteMapping("/{commentId}")
+    @DeleteMapping("/{adId}/comments/{commentId}")
 //    public void deleteComment(@PathVariable int adId , @RequestParam int commentId ) {
     public void deleteComment(@PathVariable("commentId") int pk ) {
       commentsService.deleteComment(pk);
@@ -51,7 +51,7 @@ public class CommentsController {
 
     // обновление комментария
         @Operation(summary = "Обновление комментария")
-        @PatchMapping("{commentId}")
+        @PatchMapping("/{adId}/comments/{commentId}")
     public ResponseEntity<CommentDTO> updateComment(@PathVariable int adId,@PathVariable("commentId") int pk,
                                                     @RequestBody CreateOrUpdateCommentDTO COUComment) {
               return ResponseEntity.ok(commentsService.updateComment(adId, pk, COUComment));
