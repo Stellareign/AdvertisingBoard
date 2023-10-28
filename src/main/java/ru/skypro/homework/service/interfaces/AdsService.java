b86754d769d373917b9608b21d5816a611e972c9
Stellareign <tuguran@mail.ru>
2023-10-28 13:41:54 +0300

    Не работает аутентификация :(

--- src/main/java/ru/skypro/homework/service/interfaces/AdsService.java
+++ src/main/java/ru/skypro/homework/service/interfaces/AdsService.java
@@ -16,6 +16,7 @@ public interface AdsService {
 
     public abstract ExtendedAdDTO getAdById(int adsId);
 
+
     //+++++++++++++++++++++++++++++++++++++++++
     public abstract void deleteAdsById(int adsId) throws IOException;
 
